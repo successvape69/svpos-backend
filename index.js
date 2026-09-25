0a34f3c6de5252debe38fb548a2b40409f614d52
Baron <baron@svpos.local>
2026-09-25 16:09:48 +0700

    Add static frontend hosting on /

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ require('dotenv').config();
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static('public'));
 
 // MongoDB connect
 mongoose.connect(process.env.MONGO_URI)
